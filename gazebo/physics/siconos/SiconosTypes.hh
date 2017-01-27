bf4112bd0c8bcadb16f3c5f5788e6552bc1e88fb
Stephen Sinclair <stephen.sinclair@inria.cl>
2017-01-27 18:56:15 -0300

    Vector conversions for SiconosVector and ignition.

--- gazebo/physics/siconos/SiconosTypes.hh
+++ gazebo/physics/siconos/SiconosTypes.hh
@@ -19,6 +19,8 @@
 #define _SICONOSTYPES_HH
 
 #include <boost/shared_ptr.hpp>
+#include <boost/make_shared.hpp>
+#include <SiconosVector.hpp>
 
 /// \file
 /// \ingroup gazebo_physics
@@ -37,6 +39,77 @@ namespace gazebo
     typedef boost::shared_ptr<SiconosLink> SiconosLinkPtr;
     typedef boost::shared_ptr<SiconosPhysics> SiconosPhysicsPtr;
     typedef boost::shared_ptr<SiconosSurfaceParams> SiconosSurfaceParamsPtr;
+
+    class GZ_PHYSICS_VISIBLE SiconosTypes {
+      /// \brief Convert a SiconosVector to a gazebo Vector3.
+      /// \param[in] _bt SiconosVector of size 3.
+      /// \return Gazebo Vector3.
+      public: static ignition::math::Vector3d ConvertVector3(SP::SiconosVector _v)
+              {
+                assert(_v->size() >= 3);
+                return ignition::math::Vector3d((*_v)(0), (*_v)(1), (*_v)(2));
+              }
+
+      /// \brief Convert a gazebo Vector3 to a SiconosVector.
+      /// \param[in] _vec Gazebo Vector3.
+      /// \return SiconosVector of size 3.
+      public: static SP::SiconosVector ConvertVector3(
+        const ignition::math::Vector3d &_vec)
+              {
+                auto v = std11::make_shared<SiconosVector>(3);
+                (*v)(0) = _vec.X();
+                (*v)(1) = _vec.Y();
+                (*v)(2) = _vec.Z();
+                return v;
+              }
+
+      /// \brief Convert a SiconosVector(7) to a gazebo pose.
+      /// \param[in] _v SiconosVector of size 7.
+      /// \return Gazebo pose.
+      public: static ignition::math::Pose3d ConvertPose(SP::SiconosVector _v)
+              {
+                ignition::math::Pose3d pose;
+                pose.Pos() = ConvertVector3(_v);
+                pose.Rot().W() = (*_v)(3);
+                pose.Rot().X() = (*_v)(4);
+                pose.Rot().Y() = (*_v)(5);
+                pose.Rot().Z() = (*_v)(6);
+                return pose;
+              }
+
+      /// \brief Convert a gazebo pose to a new SiconosVector(7)
+      /// \param[in] _pose Gazebo pose.
+      /// \return SP::SiconosVector pose (size 7).
+      public: static SP::SiconosVector ConvertPose(const ignition::math::Pose3d &_pose)
+              {
+                auto v = std11::make_shared<SiconosVector>(7);
+                (*v)(0) = _pose.Pos().X();
+                (*v)(1) = _pose.Pos().Y();
+                (*v)(2) = _pose.Pos().Z();
+                (*v)(3) = _pose.Rot().W();
+                (*v)(4) = _pose.Rot().X();
+                (*v)(5) = _pose.Rot().Y();
+                (*v)(6) = _pose.Rot().Z();
+                return v;
+              }
+
+      /// \brief Convert a gazebo pose to an existing SiconosVector(7)
+      /// \param[in] _pose Gazebo pose.
+      /// \return SP::SiconosVector pose (size 7).
+      public: static SP::SiconosVector ConvertPoseToVector7(
+        const ignition::math::Pose3d &_pose,
+        SP::SiconosVector _v)
+              {
+                (*_v)(0) = _pose.Pos().X();
+                (*_v)(1) = _pose.Pos().Y();
+                (*_v)(2) = _pose.Pos().Z();
+                (*_v)(3) = _pose.Rot().W();
+                (*_v)(4) = _pose.Rot().X();
+                (*_v)(5) = _pose.Rot().Y();
+                (*_v)(6) = _pose.Rot().Z();
+                return _v;
+              }
+    };
   }
 }
 #endif  // #ifndef _SICONOSTYPES_HH
