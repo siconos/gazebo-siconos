cdfc37194b2d3b91a9792467398168eff41b2671
Stephen Sinclair <stephen.sinclair@inria.cl>
2017-08-11 15:25:31 -0400

    Merge default

--- gazebo/physics/Link.cc
+++ gazebo/physics/Link.cc
@@ -250,6 +250,8 @@ void Link::Init()
 //////////////////////////////////////////////////
 void Link::Fini()
 {
+  this->updateConnection.reset();
+
   this->attachedModels.clear();
   this->parentJoints.clear();
   this->childJoints.clear();
@@ -688,6 +690,11 @@ ignition::math::Vector3d Link::RelativeLinearAccel() const
 //////////////////////////////////////////////////
 ignition::math::Vector3d Link::WorldLinearAccel() const
 {
+  // Developer note (MXG): The meaning of WorldForce seems to vary between
+  // engines. In particular, ODE sees it as the net force on a body, whereas
+  // DART sees it as the net of the external forces (which excludes internal
+  // forces caused by joint constraints). These will be equivalent for bodies
+  // that have no joints, but they will not be equivalent in general.
   return this->WorldForce() / this->inertial->Mass();
 }
 
